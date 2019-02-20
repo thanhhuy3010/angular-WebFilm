54e4a07045296c7ed71bc8a135a832e3ead6e5ba
Nhật Tiến Châu <nhattien24071996@gmail.com>
2019-02-20 15:11:01 +0700

    version_1

--- src/app/home/homepage-module/movie-playing/movie-playing.component.ts
+++ src/app/home/homepage-module/movie-playing/movie-playing.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit } from '@angular/core';
+declare var Swiper: any;
 
 @Component({
   selector: 'app-movie-playing',
   templateUrl: './movie-playing.component.html',
   styleUrls: ['./movie-playing.component.scss']
 })
-export class MoviePlayingComponent implements OnInit {
+export class MoviePlayingComponent implements OnInit ,AfterViewInit {
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) {
+    
+   }
 
   ngOnInit() {
+    
   }
+
+  ngAfterViewInit () {
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      slidesPerColumn: 2,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
+  
+ 
+ 
+  
 
 }
--- src/app/home/homepage-module/navbar/navbar.component.ts
+++ src/app/home/homepage-module/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import $ from 'jquery';
+declare var $:any;
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +12,28 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function(){
+      $(".navbar-toggler, .overlay").on("click",function(){
+        $(".mobileMenu, .overlay" ).toggleClass("open");
+      })
+    });
+    
+    $(window).scroll(function(){
+      if($(window).scrollTop() > $('#carouselId').position().top){
+          $('.navbar').addClass('header-scroll')
+      }
+      else{
+          $('.navbar').removeClass('header-scroll')
+      }
+    })
+
   }
+
+  toggle_div() {
+    var ele = document.getElementById('sidebar');
+    ele.classList.toggle('active');
+  };
+  
+  
 
 }
